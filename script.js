2eb1a38558381349b1a4ea53b80fd351a37da36c
AlpaChinoCZ <michal.dvorak99@seznam.cz>
2024-11-26 21:37:52 +0100

    basic port site added

--- script.js
+++ script.js
@@ -20,3 +20,11 @@ document.getElementById('search-close-btn').onclick = function() {
         elements[i].classList.toggle('disable-nav')
     }
 }
+
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
